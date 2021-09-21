f31bd93edee064a583501a2ca90f30dbfeb46f47
pmita <panos.mita2@outlook.com>
2021-09-21 14:52:17 +0100

    Feat/Banner.js added as a dumb component

--- src/featured/Banner/component.js
+++ src/featured/Banner/component.js
@@ -1,11 +1,29 @@
 import React from 'react';
+// import styles
+import './style.scss';
+import { Link } from 'react-router-dom';
 
-const component = () => 
+const component = ({
+	bannerTitleOne = 'Sample Title Vol.1',
+	bannerTitleTwo = 'Sample Title vol.2',
+	bannerText = 'Sample Text goes here',
+	bannerImage,
+	bannerLink = '#'
+}) => 
 {
     return (
-	<div>
-		<h1>Hello from the Banner component</h1>
-	</div>
+	<section className="banner-section">
+		<div className="banner-left">
+			<h1>{bannerTitleOne} <br />& {bannerTitleTwo}</h1>
+			<p>
+				{bannerText}
+			</p>
+		</div>
+		<div className="banner-right">
+			<img src={bannerImage} alt="depiction of our model as part of the banner section" />
+			<Link to={bannerLink}>See more here</Link>
+		</div>
+	</section>
     );
 };
 
--- src/pages/Home/component.js
+++ src/pages/Home/component.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './style.scss';
+// import our components
+import Banner from '../../featured/Banner';
+// import assets
+import bannerImg from '../../assets/images/bannerImg.png';
 
 const component = () =>
 {
     return (
 	<div>
-		<h1>hello from Home page</h1>
+		<Banner 
+			bannerTitleOne='Elegance'
+			bannerTitleTwo='Comfort'
+			bannerText='Our new Range of overcoats made from 100% 
+			cashmere, ethically sourced and without the 
+			price tage of old fashion houses.'
+			bannerImage={bannerImg}
+			bannerLink='/women'
+		/>
 	</div>
     );
 };
